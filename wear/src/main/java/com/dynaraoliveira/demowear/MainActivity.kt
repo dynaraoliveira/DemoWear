a0adb343439274c432554b671541732a8db5efdb
logonrmlocal <dynara.oliveira@gmail.com>
2019-04-10 22:56:51 -0300

    Aula 1 Wear

--- wear/src/main/java/com/dynaraoliveira/demowear/MainActivity.kt
+++ wear/src/main/java/com/dynaraoliveira/demowear/MainActivity.kt
@@ -3,9 +3,28 @@ package com.dynaraoliveira.demowear
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import android.widget.Toast
+import com.dynaraoliveira.shared.Carro
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.Wearable
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : WearableActivity() {
+class MainActivity : WearableActivity(),
+        GoogleApiClient.ConnectionCallbacks {
+
+    private lateinit var client: GoogleApiClient
+
+    override fun onConnected(p0: Bundle?) {
+        Wearable.MessageApi.addListener(client) {
+            val carro = Gson()
+                    .fromJson(String(it.data), Carro::class.java)
+            Toast.makeText(this, carro.modelo, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +36,12 @@ class MainActivity : WearableActivity() {
         btCliqueAqui.setOnClickListener {
             Toast.makeText(this, "Fui clicado", Toast.LENGTH_LONG).show()
         }
+
+        client = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(Wearable.API)
+                .build()
+        client.connect()
     }
 }
 
